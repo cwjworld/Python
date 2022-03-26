ad25738fbc5dbb0849337792b527d17ff1b9a1a6
cwjworld <564521556@qq.com>
2022-03-26 15:47:55 +0800

    表格

---
+++ mypy08.py
@@ -0,0 +1,16 @@
+r1 = {"name":"高小一","age":18,"salary":30000,"city":"北京"}
+r2 = {"name":"高小二","age":19,"salary":20000,"city":"上海"}
+r3 = {"name":"高小三","age":20,"salary":30000,"city":"深圳"}
+
+tb = [r1,r2,r3]
+
+#获得第二行的的人的薪资
+print(tb[1].get("salary"))
+
+#打印表中所有薪资
+for i in range(len(tb)):
+    print(tb[i].get("salary"))
+
+#打印表格的所有数据
+for i in range(len(tb)):
+    print(tb[i].get("name"),tb[i].get("age"),tb[i].get("salary"),tb[i].get("city"),)  
