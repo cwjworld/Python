070b3f587b2f6449c2eb4507c6fd4527e1ea6ab1
cwjworld <564521556@qq.com>
2022-05-03 23:48:53 +0800

    直接调用操作系统可执行文件

---
+++ test.os/my01.py
@@ -0,0 +1,9 @@
+import os
+
+# os.system("notepad.exe")
+# os.system("regedit")
+# os.system("ping www.baidu.com")
+# os.system("cmd")
+
+# 调用操作系统可执行文件
+os.startfile(r"C:\Program Files (x86)\WeGame\wegame.exe")
